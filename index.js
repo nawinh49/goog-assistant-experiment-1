42bfc0d508d4a0d7cf565fe3a78c35d3fe58b3cb
wilac <39047467+wilac@users.noreply.github.com>
2018-05-18 09:50:19 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
-const { SimpleResponse, Carousel, Image } = require('dialogflow-fulfillment/node_modules/actions-on-google/dist/service/actionssdk');
+/*const { SimpleResponse, Carousel, Image } = require('dialogflow-fulfillment/node_modules/actions-on-google/dist/service/actionssdk');*/
 
+const {  dialogflow,  BasicCard,  BrowseCarousel, BrowseCarouselItem , Button,  Carousel,  Image,  LinkOutSuggestion,  List,  MediaObject,  Suggestions,  SimpleResponse } = require('dialogflow-fulfillment/node_modules/actions-on-google/dist/service/actionssdk');
 const processor = require('./processor')
 const express = require('express'),
     bodyParser = require('body-parser')
