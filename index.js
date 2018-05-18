f7e269bb6001bfdac6d79e171ba4e3bcd95099ce
wilac <39047467+wilac@users.noreply.github.com>
2018-05-18 09:51:13 +0700

    Update index.js

--- index.js
+++ index.js
@@ -157,41 +157,37 @@ app.post('/', (req, res) => {
             text: greeting
         }))
         
-        conv.ask(new Carousel({
-            items: {
-                'Select_399': {
-                    
-                    title: packagename1,
-                    
-                    description: packagedetail1,
-                    
-                    image: new Image({
-                        url: simImg[0], alt: packagename1,
-                        
-                    }) 
-                  
-
-                },
-                'Select_899': {
-                    title: packagename2 ,
-                    description: packagedetail2 ,
-                   
-                    image: new Image({
-                        url: simImg[1], alt: packagename2,
-                       
-                    })
-                },
-                'Select_600': {
-                    title: packagename3 ,
-                    description: packagedetail3 ,
-                    
-                    image: new Image({
-                        url: simImg[2], alt: packagename3,
-                       
-                    })
-                }
-            }
-        }))
+        conv.ask(new BrowseCarousel({
+          items: [
+            new BrowseCarouselItem({
+              title: packagename1,
+              url: 'http://www.ais.co.th/',
+              description: packagedetail1,
+              image: new Image({
+                 url: simImg[0], alt: packagename1,
+              }),
+              footer: 'Item 1 footer',
+            }),
+            new BrowseCarouselItem({
+               title: packagename2,
+               url: 'http://www.ais.co.th/',
+              description: packagedetail2,
+              image: new Image({
+                 url: simImg[1], alt: packagename2,
+              }),
+              footer: 'Item 2 footer',
+            }),
+             new BrowseCarouselItem({
+               title: packagename3,
+               url: 'http://www.ais.co.th/',
+              description: packagedetail3,
+              image: new Image({
+                 url: simImg[2], alt: packagename3,
+              }),
+              footer: 'Item 2 footer',
+            }),  
+          ],
+        }));
         agent.add(conv)
     }
     
